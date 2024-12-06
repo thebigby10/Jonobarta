7695c7f427d7ac82784a7dce3d14052c88ebffb0
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-12-06 09:34:15 +0600

    updated db to mysql

--- backend/jonobarta_backend/settings.py
+++ backend/jonobarta_backend/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'jonobarta_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+            "ENGINE": "django.db.backends.mysql",
+            # replace these with your configuration
+            "NAME": "jonobarta_db",
+            "USER": "admin",
+            "PASSWORD": "admin",
+            "HOST": "127.0.0.1",
+            "PORT": "3306",
+        }
 }
 
 
