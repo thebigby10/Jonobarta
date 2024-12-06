e15214ec2dc4224cbfed764b667b18f6e1252586
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-12-06 10:09:52 +0600

    updated dummy api

--- backend/jonobarta_backend/settings.py
+++ backend/jonobarta_backend/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders.middleware.CorsMiddleware',
     'corsheaders',
     'user_management',
     'comment',
@@ -42,6 +41,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
