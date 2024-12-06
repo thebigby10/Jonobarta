504d1f846c97cd86f17149eb51690dbbc7e98d29
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-12-06 09:12:48 +0600

    created django apps

---
+++ backend/crime_stat/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrimeStatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crime_stat'
---
+++ backend/transparent_market/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TransparentMarketConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'transparent_market'
